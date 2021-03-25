b16c60041e707ba0ab59066189db412b057b2a4f
Cristiane Santiago <cris.s.santiago@gmail.com>
2021-03-25 18:58:25 -0400

    Directed mysql connection to the server on amazon

--- Startup.cs
+++ Startup.cs
@@ -38,16 +38,13 @@ namespace RestAPI
             // string value = ConfigurationManager.ConnectionStrings[databaseConnection].ConnectionString;
 
             services.AddDbContext<RestAPIContext>(options =>
-                options.UseMySql(
+                options.UseMySql(                    
 
-                    // Kaelen Connection
-                    // "server=localhost;database=myapp_development;uid=root;password=Pa$$w0rd!",
-
-                    // Cristiane Connection
-                    "server=localhost;port=3306;database=RailsApp_development;uid=codeboxx;password=Codeboxx1*",
+                    // Cristiane Connection local
+                    //"server=localhost;port=3306;database=RailsApp_development;uid=codeboxx;password=Codeboxx1*",
 
                     // Live Site Connection
-                    //"server=codeboxx.cq6zrczewpu2.us-east-1.rds.amazonaws.com;database=KaelenBurroughs;uid=codeboxx;password=Codeboxx1!",
+                    "server=codeboxx.cq6zrczewpu2.us-east-1.rds.amazonaws.com;database=CristianeSantiago;uid=codeboxx;password=Codeboxx1!",
 
                     new MySqlServerVersion(new Version(8, 0, 21)),
                         mySqlOptions => mySqlOptions
